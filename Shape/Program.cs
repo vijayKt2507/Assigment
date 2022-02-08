23cee875fbef63ccda8f2e75f14cd672308d04d4
vijayKt2507 <99236954+vijayKt2507@users.noreply.github.com>
2022-02-08 11:17:11 +0530

    Add files via upload

---
+++ Shape/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    class Shape
+    {
+
+        private string color;
+        private Boolean filled;
+        public Shape()
+        {
+            filled = true;
+            color = "green";
+        }
+        public Boolean isFilled()
+        {
+            if (filled == true)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        
+
+
+
+    }
+    class circle : Shape
+    {
+       double b = 3.15;
+        
+
+        public void  AreaOfCircle(int A)
+        {
+          // A=πr2
+        
+
+        }
+     }
+    class Rectangle:Shape
+    {
+        public void AreaOfrRectagle(int len,int width)
+        {
+
+            int AreaRectagle = len * width;
+        }
+
+    }
+    class Square
+    {
+        public void AreaOfSquare(int a)
+        {
+            int AreaSquare =Convert.ToInt32( Math.Sqrt(a));
+
+
+        }
+
+    }
+    
+
+    }
+
+
+
+
+
+
