9a61231d66480300785936aa2eaa1677495cbff3
vijayKt2507 <99236954+vijayKt2507@users.noreply.github.com>
2022-02-08 10:44:32 +0530

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment_person
+{
+    class Person
+    {
+
+        static void Main(string[] args)
+        {
+
+            Student s = new Student("vijay", "bangalore", "karnataga", "fundamentails", 2010, 4500);
+            s.studentDetails();
+            
+            Staff s1 = new Staff("anil", "chennai","tamilnadu","gbhss",1234);
+           
+            s1.displayStaffDetails();
+
+     //Person[] people = { new Student("vijay", "bangalore", "karnataga", "fundamentails", 2010, 4500),
+     //                     new Staff("anil","chennai","tamilnadu","gnn",223};
+          
+           
+
+
+
+
+
+        
+
+
+
+
+
+
+
+                    
+        }
+    }
+}
