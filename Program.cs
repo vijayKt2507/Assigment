3ffe98be9c12246bf766715c005f5071a58a0562
vijayKt2507 <99236954+vijayKt2507@users.noreply.github.com>
2022-02-08 10:42:58 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,55 +1,21 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ArrayToCsv
+namespace ascending_order
 {
     class Program
     {
-      
-
         static void Main(string[] args)
         {
-            string[,] files = new string[2, 5];
-            files[0, 0] = "Hii";
-            files[0, 1] = "this";
-            files[0, 2] = "vijay";
-            files[0, 3] = "dfh";
-            files[0, 4] = "rit";
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
 
-            files[1, 0] = "asfhjfjfh";
-            files[1, 1] = "43";
-            files[1, 2] = "76";
-            files[1, 3] = "bangalore";
-            files[1, 4] = "6533108";
-
-            StreamWriter sw = File.CreateText("OutputText.csv");
-            for(int i=0;i<2;i++)
-
-            {
-                for(int j=0;j<5;j++)
-                {
-                    sw.Write(files[i, j] + ";");
-
-                }
-
-                sw.Write("\n");
-
-            }
-
-            sw.Flush();
-            sw.Close();
-            Console.WriteLine("success");
-          
-
-
-
-
-
-
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
